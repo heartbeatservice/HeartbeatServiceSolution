f529a75b4d07458fec4beeb5e19aff05a5bf4f0d
heartbeatservice <saqib@heartbeat-biz.com>
2014-03-18 00:02:03 -0400

    added/updated database schema
    added/updated database schema

---
+++ HBS.Entities/ConfigItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HBS.Entities
+{
+    public class ControlConfig
+    {
+        public int ConfigId { get; set; }
+        public int CompnayId { get; set; }
+        public string CompnayName { get; set; }
+        public string ControlKey { get; set; }
+        public string ControlValue { get; set; }
+        public float ControlOrder { get; set; }
+        public List<ControlConfigItem> ControlConfigItemList { get; set; }
+        public string OtherText { get; set; }
+
+        public ControlConfig()
+        {
+            ControlConfigItemList = new List<ControlConfigItem>();
+        }
+    }
+}
