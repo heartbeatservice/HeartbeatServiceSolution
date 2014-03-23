92e9e25b996eb2dd194e9a0cfba10f0fe02940be
Umais Siddiqui <umais20@yahoo.com>
2014-03-23 02:21:05 -0400

    Added Website template to website project

--- HBS.Website/Controllers/HomeController.cs
+++ HBS.Website/Controllers/HomeController.cs
@@ -4,29 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace HBS.Website.Controllers
+namespace HeartbeatWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Team()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
     }
 }
