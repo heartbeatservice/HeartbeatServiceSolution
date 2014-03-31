2dffd018a360c9cbc51bbb420ac550f6553ff58c
Saqib <umais20@yahoo.com>
2014-03-31 01:26:00 -0400

    Added repository

---
+++ HBS.Data/Abstract/IProfessionalRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HBS.Entities;
+
+namespace HBS.Data.Abstract
+{
+    public interface IProfessionalRepository
+    {
+        bool AddProfessional(Professional professional);
+        bool UpdateProfessional(Professional professional);
+        Professional GetProfessional(int professionalId);
+        List<Professional> GetProfessional(int companyId, string professionalName);
+        bool RemoveProfessional(int professionalId);
+
+
+        bool AddProfessionalSchedule(ProfessionalSchedule professionalSchedule);
+        bool UpdateProfessionalSchedule(ProfessionalSchedule professionalSchedule);
+        ProfessionalSchedule GetProfessionalSchedule(int professionalSchedulreId);
+        List<ProfessionalSchedule> GetProfessionalScheduleList(int professionalId);
+        List<ProfessionalSchedule> GetProfessionalSchedule(DateTime scheduleDate);
+        bool RemoveProfessionalSchedule(int professionalSchduleId);
+
+
+
+    }
+}
---
+++ HBS.Data/Concrete/CustomerRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HBS.Data.Abstract;
+using HBS.Entities;
+
+namespace HBS.Data.Concrete
+{
+    public class CustomerRepository : BaseRepository, ICustomerRepository
+    {
+        public bool AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers(int companyId, string customerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers(int companyId, string customerName, DateTime Dob)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddCustomerInsurance(CustomerInsurance customerInsurance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomerInsurance(CustomerInsurance customerInsurance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerInsurance GetCustomerInsurance(int customerInsuranceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerInsurance> GetCustomerInsurances(int companyId, string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveCustomerInsurance(int customerInsuranceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
