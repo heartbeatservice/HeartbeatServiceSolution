9d072f583be24c008b2ff4485c045f0e126fa1c4
Saqib <najamsaqib@gmail.com>
2014-04-06 21:42:42 -0400

    Customer SPs done by Naveed synced by Saqib

--- HBS.Database.HeartbeatService/dbo/Tables/CustomerInsurance.sql
+++ HBS.Database.HeartbeatService/dbo/Tables/CustomerInsurance.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[CustomerInsurance] (
-    [InsuranceId]             INT            NOT NULL,
+    [CustomerInsuranceID]     INT            IDENTITY (1, 1) NOT NULL,
+    [InsuranceID]             INT            NOT NULL,
     [CustomerId]              INT            NOT NULL,
     [EffectiveDate]           DATE           NOT NULL,
     [EndDate]                 DATE           NULL,
     [PCPName]                 NVARCHAR (100) NULL,
     [CustomerInsuranceNumber] NVARCHAR (50)  NULL,
     [InsuranceType]           NVARCHAR (50)  NULL,
-    CONSTRAINT [PK_Customer_Insurance] PRIMARY KEY CLUSTERED ([InsuranceId] ASC, [CustomerId] ASC, [EffectiveDate] ASC)
+    CONSTRAINT [PK_CustomerInsurance] PRIMARY KEY CLUSTERED ([CustomerInsuranceID] ASC)
 );
+
+
 
