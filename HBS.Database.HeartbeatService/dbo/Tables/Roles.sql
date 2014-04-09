747b282787f13f9fa583955de8092615a5ed2245
naveed84n@gmail.com <naveed84n@gmail.com>
2014-04-08 23:58:19 -0400

    Check-in for Roles related SPs

--- HBS.Database.HeartbeatService/dbo/Tables/Roles.sql
+++ HBS.Database.HeartbeatService/dbo/Tables/Roles.sql
@@ -1,10 +1,12 @@
-﻿CREATE TABLE [dbo].[Roles]
-(
-	[RoleId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [CompanyId] INT NOT NULL, 
-    [RoleName] VARCHAR(150) NOT NULL, 
-    [CreatedBy] INT NULL, 
-    [CreatedDate] DATETIME NULL, 
-    [UpdatedBy] INT NULL, 
-    [UpdatedOn] DATETIME NULL
-)
+﻿CREATE TABLE [dbo].[Roles] (
+    [RoleId]      INT           IDENTITY (1, 1) NOT NULL,
+    [CompanyId]   INT           NOT NULL,
+    [RoleName]    VARCHAR (150) NOT NULL,
+    [CreatedBy]   INT           NULL,
+    [CreatedDate] DATETIME      NULL,
+    [UpdatedBy]   INT           NULL,
+    [DateUpdated] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([RoleId] ASC)
+);
+
+
