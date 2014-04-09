b60fb73eaaab094917639866274a899c57677503
Umais Siddiqui <umais20@yahoo.com>
2014-04-09 19:05:10 -0400

    Added SP's for Appointment and wrapped up Schedules

---
+++ HBS.Database.HeartbeatService/dbo/Stored Procedures/GetCustomerAppointments.sql
@@ -0,0 +1,22 @@
+﻿
+CREATE ProCedure [dbo].[GetCustomerAppointments]
+@CustomerID int,
+@StartDate Date=NULL,
+@EndDate Date=NULL
+
+AS
+
+If @StartDate is null
+Begin
+SET @EndDate =NULL
+
+SELECT * FROM Appointments where CustomerId=@CustomerID
+END
+ELSE
+Begin
+IF @EndDate is null
+SET @EndDate=DATEADD(DD,1,@StartDate)
+
+
+SELECT * FROM Appointments Where CustomerId=@CustomerID AND AppointmentDate between @StartDate And @EndDate
+END
---
+++ HBS.Database.HeartbeatService/dbo/Stored Procedures/GetProfessionalAppointments.sql
@@ -0,0 +1,23 @@
+﻿
+
+CREATE ProCedure [dbo].[GetProfessionalAppointments]
+@ProfessionalID int,
+@StartDate Date=NULL,
+@EndDate Date=NULL
+
+AS
+
+If @StartDate is null
+Begin
+SET @EndDate =NULL
+
+SELECT * FROM Appointments where ProfessionalId=@ProfessionalID
+END
+ELSE
+Begin
+IF @EndDate is null
+SET @EndDate=DATEADD(DD,1,@StartDate)
+
+
+SELECT * FROM Appointments Where ProfessionalId=@ProfessionalID AND AppointmentDate between @StartDate And @EndDate
+END
