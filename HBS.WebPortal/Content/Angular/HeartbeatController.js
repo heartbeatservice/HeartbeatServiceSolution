1a8778609777146f6a84b1c5327f998b659f19ba
Umais Siddiqui <umais20@yahoo.com>
2014-04-15 01:28:47 -0400

    Added SubmenuItems

--- HBS.WebPortal/Content/Angular/HeartbeatController.js
+++ HBS.WebPortal/Content/Angular/HeartbeatController.js
@@ -1,6 +1,6 @@
 ﻿HeartbeatApp.controller("HeartbeatController", function AppController($scope, $location, HeartbeatService) {
    
-    $scope.menuItems = [{ name: 'Home', cls: 'nav active', url: 'home/index' }, { name: 'About Us', cls: 'nav', url: 'home/about' , submenu:[{name:'Vision' }] }, { name: 'Contact Us', cls: 'nav', url: 'Home/Contact' }];
+    $scope.menuItems = [{ name: 'Home', cls: 'nav active', url: 'home/index' }, { name: 'About Us', cls: 'nav', url: 'home/about', submenu: [{ name: 'Vision', url: 'home/about' }, { name: 'Meet The Team', url: 'home/team' }] }, { name: 'Contact Us', cls: 'nav', url: 'Home/Contact' }];
 
 
     $scope.url = $location.host();
@@ -13,6 +13,8 @@
     $scope.loginSucess = function (response) {
         $scope.User = response;
         if ($scope.User.UserId > 0) {
+            $scope.menuItems = [{ name: 'Schedule', cls: 'nav active', url: 'home/index' }, { name: 'Appointment', cls: 'nav', url: 'home/about', submenu: [{ name: 'Vision', url: 'home/about' }, { name: 'Meet The Team', url: 'home/team' }] }, { name: 'Admin', cls: 'nav', url: 'Home/Contact' }];
+
             $scope.constructMenu();
             $scope.$apply();
            // window.location.href = "/home/about";
@@ -35,11 +37,34 @@
     $scope.constructMenu = function () {
         var mainMenu = document.getElementById('menu');
         var menuItem;
+        var submenu;
+        var submenuitem;
         mainMenu.innerHTML='';
         for (var i = 0; i < $scope.menuItems.length; i++) {
+
             menuItem = document.createElement('li');
-            menuItem.setAttribute("class", "nav");
-            menuItem.innerHTML = '<a href=http://'+$scope.url+':'+$scope.port+'/'+$scope.menuItems[i].url+'/>'+$scope.menuItems[i].name+'</a>';
+            if ($scope.menuItems[i].submenu === undefined) {
+
+
+                menuItem.setAttribute("class", "nav");
+                menuItem.innerHTML = '<a href=http://' + $scope.url + ':' + $scope.port + '/' + $scope.menuItems[i].url + '/>' + $scope.menuItems[i].name + '</a>';
+            }
+            else {
+                menuItem.setAttribute("class", "dropdown");
+                
+                menuItem.onmouseover = function () { this.setAttribute("class", "dropdown open"); };
+                menuItem.onmouseout = function () { this.setAttribute("class", "dropdown"); };
+                menuItem.innerHTML = '<a data-toggle=dropdown class=dropdown-toggle href=http://' + $scope.url + ':' + $scope.port + '/' + $scope.menuItems[i].url + '/>' + $scope.menuItems[i].name + '<b class=caret></b></a>';
+                submenu = document.createElement('ul');
+                submenu.setAttribute("class", "dropdown-menu");
+                for (j = 0; j < $scope.menuItems[i].submenu.length; j++)
+                {
+                    submenuitem = document.createElement('li');
+                    submenuitem.innerHTML = '<a  href=http://' + $scope.url + ':' + $scope.port + '/' + $scope.menuItems[i].submenu[j].url + '/>' + $scope.menuItems[i].submenu[j].name + '</a>';
+                   submenu.appendChild(submenuitem);
+                }
+                menuItem.appendChild(submenu);
+            }
             mainMenu.appendChild(menuItem);
         }
 
