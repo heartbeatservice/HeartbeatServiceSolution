2ca080b7cd62983d27b6f9b32ddfd488c4837d53
Umais Siddiqui <umais20@yahoo.com>
2014-05-07 08:18:11 -0400

    ui changes

--- HBS.WebPortal/Content/Angular/SchedulingController.js
+++ HBS.WebPortal/Content/Angular/SchedulingController.js
@@ -1,5 +1,5 @@
 ﻿HeartbeatApp.controller("SchedulingController", function AppController($scope, $location, HeartbeatService) {
-    $scope.page = "Scheduling";
+    $scope.page = "Heartbeat Service Portal";
     $scope.ProviderDashboard = [{ doctor: 'Farzana Aziz', patients: 23 }, { doctor: 'Abbas Rizvi', patients: 12 }];
     $scope.ReminderDashboard = ['23 Patients have to be reminded of an upcoming appointment', "Dr. Farzana's Schedule has to be updated"]
 });
